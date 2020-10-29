508d8adff7d75fecbee31fea9b7613106f0a8c25
Marcin Medryk <medryk.kontakt@gmail.com>
2020-10-29 18:04:43 +0100

    CzarnaMagia

--- src/App.js
+++ src/App.js
@@ -3,21 +3,7 @@ import React from "react";
 import MyInput from "./MyInput";
 
 function App() {
-  let [shopItemInput, setShopItemInput] = React.useState("");
-  let test1 = function () {
-    setShopItemInput("");
-  };
-
-  let onChange1 = (e) => {
-    setShopItemInput(e.currentTarget.value);
-    console.log(shopItemInput);
-  };
-
-  return (
-    <div className="App">
-      {<MyInput onChange={onChange1} test={test1} value={shopItemInput} />}
-    </div>
-  );
+  return <div className="App">{<MyInput />}</div>;
 }
 
 export default App;
--- src/MyInput.js
+++ src/MyInput.js
@@ -1,10 +1,31 @@
 import React from "react";
 
 function MyInput(props) {
+  let [shopItemInput, setShopItemInput] = React.useState("Value");
+  let [shopItem, setShopItem] = React.useState("");
+  let [listNumber, setListNumber] = React.useState(1);
   return (
     <div>
-      <input onChange={props.onChange} value={props.value}></input>
-      <button onClick={props.test}>Testing</button>
+      <input
+        onChange={(e) => setShopItemInput(e.currentTarget.value)}
+        value={shopItemInput}
+      ></input>
+      <button
+        onClick={() => {
+          setShopItemInput([""]);
+          setListNumber(listNumber + 1);
+          setShopItem([
+            ...shopItem,
+            <div className={listNumber}>
+              {shopItemInput}
+              <button>Tekst</button>
+            </div>,
+          ]);
+        }}
+      >
+        Testing
+      </button>
+      <p>{shopItem}</p>
     </div>
   );
 }
